ed66f071e67c006d40b4956b53a718aae93eacf7
Adriano Ruseler <adrianoruseler@gmail.com>
2024-06-03 13:24:40 -0300

    Added Filter03

---
+++ TE10/LTspice/filter03func3.m
@@ -0,0 +1,12 @@
+function  out = filter03func3(parvalues) % Function 1 for filter 01
+
+% circuit.parnamesim={'Vs','R1','R2','C1','C2','RL'}; % Variables names
+
+Vs = parvalues(1);
+R1 = parvalues(2);
+R2 = parvalues(3);
+RL = parvalues(6);
+C1 = parvalues(4);
+C2 = parvalues(5);
+
+out = 1/(R1*C1); % w0
